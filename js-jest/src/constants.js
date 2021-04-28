f5b56e5d31b2889dfa0c62c8eab89e9813aa913d
Aaron Harris <aamharris@gmail.com>
2021-04-27 19:52:06 -0500

    add constants file

---
+++ js-jest/src/constants.js
@@ -0,0 +1,12 @@
+const SULFURAS = "Sulfuras, Hand of Ragnaros";
+const AGED_BRIE = "Aged Brie";
+const BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+
+const MAX_QUALITY = 50;
+
+module.exports = {
+  SULFURAS,
+  AGED_BRIE,
+  BACKSTAGE_PASSES,
+  MAX_QUALITY,
+};
